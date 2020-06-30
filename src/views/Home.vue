f3ee3655fa8758bd94219070d5b4b6666065c49f
wolf-tungsten <canis.tungsten@foxmail.com>
2020-06-30 17:11:04 +0800

    修改主办信息

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,14 +4,14 @@
       <img class="center animate__animated animate__fadeIn" width="90%;" src="../assets/欢迎页.png" />
       <div class="animate__animated animate__fadeIn" style="margin-top:30px; text-align:center; font-size:12px; animation-delay:1s">
         主办：共青团东南大学委员会<br>
-        承办：计算机科学与工程学院、<br>
-        软件学院、人工智能学院<br>
-        化学化工学院<br>
-        生物科学与医学工程学院<br>
-        材料科学与工程学院<br>
-        法学院<br>
-        协办：东南大学图书馆<br>
-        东南大学党委学工部
+        承办：共青团东南大学计算机科学与工程学院、<br>
+        软件学院、人工智能学院委员会<br>
+        共青团东南大学化学化工学院委员会<br>
+        共青团东南大学生物科学与医学工程学院委员会<br>
+        共青团东南大学材料科学与工程学院委员会<br>
+        共青团东南大学法学院委员会<br>
+        东南大学图书馆<br>
+        协办：东南大学党委学工部
       </div>
       <img @click="login" style="animation-delay:2s; width:160px; margin-top: 30px;" class="animate__animated animate__fadeIn center" src='../assets/点击开启按钮.png'/>
       <div style="font-size:12px; animation-delay:2s; margin-top: 15px;" class="animate__animated animate__fadeIn center">面向东南大学2020届本科毕业生开放</div>
